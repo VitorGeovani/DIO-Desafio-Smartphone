af4f24ecdc009b4dc637ca5fd3188e08405acbf7
Vitor Geovani <sincerev36@gmail.com>
2024-01-03 16:44:00 -0300

    Enviando os arquivos

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia");
+            Smartphone nokia = new Nokia(numero: "123456789", modelo: "3310", imei: "123456789", memoria: 16);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone Iphone");
+            Smartphone iphone = new Iphone(numero: "987654321", modelo: "12", imei: "987654321", memoria: 128);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+
+        }
+    }
+}
